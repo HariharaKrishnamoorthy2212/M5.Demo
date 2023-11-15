d9358b0967a65a250236173541bfab4421b726ad
Hari <hariharakrishnamoorthy2212@gmail.com>
2023-11-15 11:27:36 +0530

    Demo Class Pushed

---
+++ src/test/java/gitpra/Sample.java
@@ -0,0 +1,11 @@
+package gitpra;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HI welcome to globalRepository");
+
+	}
+
+}
